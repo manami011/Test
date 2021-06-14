84152ba8a36984775e1978f524c553584de29db0
manami.takeuchi2 <peettobira@gmail.com>
2021-06-14 18:30:14 +0900

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by 竹内愛実 on 2021/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
